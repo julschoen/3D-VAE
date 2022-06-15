bd91e4434cae237199ad54fa7e9141cbebabf524
julschoen <31760205+julschoen@users.noreply.github.com>
2022-06-15 14:11:35 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -131,9 +131,9 @@ class Trainer(object):
 
         self.model.zero_grad()
         with autocast():
-            rec, (commitment_loss, *_) = self.model(x)
+            rec, (commitment_loss, q,_) = self.model(x)
             rec = F.tanh(rec)
-
+        print(q.shape)
         rec_cyl, x_cyl = map(self.pre_loss_f, (rec, x))
 
         unreduced_rec_loss = F.smooth_l1_loss(rec_cyl, x_cyl, reduction='none')
@@ -149,7 +149,6 @@ class Trainer(object):
 
         for p in self.model.parameters():
             p.requires_grad = False
-        print(rec.shape, x.shape)
         return rec.detach(), rec_loss.item(), commitment_loss.item()
 
     def train(self):
