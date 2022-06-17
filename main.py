06e5f868edf1d4eb38515ecd7a327d00b669a97c
julschoen <31760205+julschoen@users.noreply.github.com>
2022-06-17 14:38:50 +0200

    Make Training more stable

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
 	parser.add_argument('--n_res_block', type=int, default=2, help='How Many Res Blocks')
 	parser.add_argument('--n_res_channel', type=int, default=32, help='ResBlock internal size')
 	parser.add_argument('--K', type=int, default=256, help='Codebook Size')
-	parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate G')
+	parser.add_argument('--lr', type=float, default=1e-5, help='Learning rate G')
 	parser.add_argument('--data_path', type=str, default='../3D-GAN/train_lidc_128.npz',help='Path to data.')
 	parser.add_argument('--val_path', type=str, default='../3D-GAN/test_lidc_128.npz',help='Path to validation data.')
 	parser.add_argument('--ngpu', type=int, default=2, help='Number of GPUs')
