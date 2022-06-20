005ef8fe2408dbbc01742427e485fa6f82026767
julschoen <31760205+julschoen@users.noreply.github.com>
2022-06-20 13:26:27 +0200

    Update model.py

--- model.py
+++ model.py
@@ -78,10 +78,4 @@ class VQVAE(nn.Module):
         dec = self.decode(quant_t, quant_b)
 
         return dec
-
-        def forward(self, input):
-        quant_t, quant_b, diff, _, _ = self.encode(input)
-        dec = self.decode(quant_t, quant_b)
-
-        return dec, diff
     
